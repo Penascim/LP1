0f5a710f993cb5cc6aedbeda46141a767e2ac377
Pedro Cardoso <142414005+Penascim@users.noreply.github.com>
2025-08-21 14:40:40 -0300

    Update ex07(test).c

--- lista1/ex07(test).c
+++ lista1/ex07(test).c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-typedef struct s_time
+typedef struct s_list
 {
         char            *time;
         int             vitoria;
         int             derrota;
         int             empate;
-        struct t_time   *next
-} t_time
+        struct s_list   *next
+} 		t_list;
 
 int main(void)
 {
@@ -17,16 +17,27 @@ int main(void)
         i = 20;
         while (i > 0)
         {
-
+		t_list	*ft_lstnew(int value)
+		{
+			t_list	*element;
+			
+			element = (t_list *)malloc(sizeof(t_list));
+			if (!element)
+				return (NULL);
+			scanf("%s\n", &element->time);
+			element->index = -1;
+			element->next = NULL;
+			return (element);
+		}
                 printf("digite o nome do time:\n");
-                scanf("%s\n", &t_time->time);
+                scanf("%s\n", &t_list->time);
                 printf("digite quantas vitorias o time possui:\n");
-                scanf("%d\n", &t_time->vitoria);
+                scanf("%d\n", &t_list->vitoria);
                 printf("digite quantas derrotas o time possui:\n");
-                scanf("%d\n", &t_time->derrota);
+                scanf("%d\n", &t_list->derrota);
                 printf("digite quantos empates o time possui:\n");
-                scanf("%d\n", &t_time->empate);
+                scanf("%d\n", &t_list->empate);
                 i--;
-
+		
         }
 }
