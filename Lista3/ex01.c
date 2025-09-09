2a24b2761d6fee821ccc6d82fa7c92961cd8504b
Pedro Cardoso <142414005+Penascim@users.noreply.github.com>
2025-09-09 14:36:38 -0300

    Implement pointer logic to compare and print values

--- Lista3/ex01.c
+++ Lista3/ex01.c
@@ -4,7 +4,21 @@ int main(void)
 {
   int a;
   int b;
+  int *p;
 
   scanf("%d", &a);
   scanf("%d", &b);
+
+  
+  if (a > b)
+    p = &a;
+  else if (a < b)
+    p = &b;
+  else
+    return (0);
+  p = p - 50;
+  printf("%d", a);
+  printf("%d", b);
+  printf("%d", p);
+  
 }
