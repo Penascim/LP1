30e179e2cbd467996889c7f65f00c046a24d4be3
Pedro Cardoso <142414005+Penascim@users.noreply.github.com>
2025-09-11 14:40:56 -0300

    ex02.c done

--- lista6-strings/ex02.c
+++ lista6-strings/ex02.c
@@ -1,36 +1,43 @@
 #include <stdio.h>
 #include <string.h>
 
+int     tamanho(char *str)
+{
+        int     i;
+
+        i = 0;
+        while(str[i] != '\0')
+                i++;
+        return (i);
+}
+
 void concatena(char* d, char* o1, char* o2)
 {
-    int i = 0;
-    int j = 0;
+    int i;
+    int j;
 
-    // Copia o1 para d
-    while (o1[i] != '\0') {
+    i = 0;
+    j = 0;
+    while (i < tamanho(o1))
+    {
         d[i] = o1[i];
         i++;
     }
-
-    // Copia o2 para d, após o1
-    while (o2[j] != '\0') {
+    while (j < tamanho(o2))
+    {
         d[i] = o2[j];
         i++;
         j++;
     }
-
-    d[i] = '\0'; // Finaliza string
+    d[i] = '\0';
 }
 
 int main(void)
 {
-    char str1[] = "Olá, ";
-    char str2[] = "mundo!";
-    char destino[100];  // Deve ser grande o suficiente para conter o1 + o2
-
+    char str1[] = "TESTE";
+    char str2[] = "aaaaaa12312345312";
+    char destino[10000];
     concatena(destino, str1, str2);
-
-    printf("Resultado: %s\n", destino);
-
+    printf("d: %s\n", destino);
     return 0;
 }
