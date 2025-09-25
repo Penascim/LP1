3e1eec283e6bc00236b384cb0e0ce55963dad980
Pedro Cardoso <142414005+Penascim@users.noreply.github.com>
2025-09-25 12:52:19 -0500

    on-build

--- lista7/ex08.c
+++ lista7/ex08.c
@@ -1,5 +1,6 @@
 #include <stdio.h> 
- 
+#include <math.h>
+
 typedef struct  s_list 
 { 
         int     x; 
@@ -7,21 +8,36 @@ typedef struct  s_list
  
 }               t_list; 
  
-void            fill_dot(t_list ponto) 
+void    fill_dot(t_list ponto)  
 { 
-        printf("ponto x:\n"); 
-        scanf("%d", ponto->x); 
-        printf("ponto y:\n"); 
-        scanf("%d", ponto->y); 
-                 
+        printf("ponto x:\n");
+        scanf("%d", &ponto->x);
+        printf("ponto y:\n");
+        scanf("%d", &ponto->y);
+             
 } 
-void            fill_vector(t_list *vet[]) 
-{ 
- 
+
+void    fill_vector(t_list vet[], int n)
+{
+        int     i;
+
+        i = 0;
+        while (i < n)
+                fill_dot(&vet[i++]);
 } 
  
-int             main(void) 
-{ 
-        t_list  vet[10]; 
-        int     i; 
+double  euclides(t_list ponto)
+{
+        return sqrt( (ponto.x * ponto.x) + (ponto.y * ponto.y) );
 }
+
+t_list  check_distance(t_list *vet[])
+{
+}
+
+int     main(void)
+{
+        t_list vet[10];
+        fill_vector(vet[10], 10);
+}
+
